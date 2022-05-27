d2407cae01bd5d9c19e88fe25b51293090b60ffa
gwartney21 <gwartneydev@gmail.com>
2022-05-26 23:30:19 -0700

    Reverted Scripts JSON file temporary
    Reverted script json temporarily added input detection

---
+++ SMARTBRAINREACTDEMO/smart-brain/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import { Component } from 'react';
+
+class App  extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      input:'',
+    }
+  }
+
+ onInputChange  = (event)=>{
+   console.log(event.target.value);
+ }
+
+ onSubmit = ()=>{
+   console.log('click');
+
+ }
+  render(){
+    return(
+    <div className="App">
+    <Navigation/> 
+     
+      <Logo/>
+     
+      <Rank/>
+
+      <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+
+         {/*
+      <FaceRecognition/> */}
+    </div>
+    )
+  };
+}
+
+export default App;
